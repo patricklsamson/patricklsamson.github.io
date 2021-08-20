5ac8dd1a87e29bc1e75e59a547e18c4359d09d44
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-20 16:18:57 +0800

    Update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ doc_ready(() => {
   scroll_class(id("nav-bar"), "bg-change");
   match_height(".mh");
 
-  match_media("only screen and (min-width: 576px)", 576, function match() {
+  match_media(576, function match() {
     match_height(".mh2");
   });
 
@@ -20,4 +20,18 @@ doc_ready(() => {
 
   click_copy(id("yahoo"));
   click_copy(id("gmail"));
+
+  add_event(id("portfolio-btn"), "click", () => {
+    toggle_class(id("portfolio-btn"), "active");
+
+    if (has_class(id("portfolio-btn"), "active")) {
+      setTimeout(() => {
+        id("portfolio-scroll-target").scrollIntoView(true);
+      }, 150);
+    } else {
+      setTimeout(() => {
+        id("portfolio").scrollIntoView(true);
+      }, 150);
+    }
+  });
 });
