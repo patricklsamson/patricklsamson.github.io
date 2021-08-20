e497ee24a624bf136352bcf55db4bb7ee7aa66b1
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-08-20 11:19:15 +0800

    Update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 doc_ready(() => {
+  let submitted = false;
+
   nav_bar();
   fixed_el(".h-add", ".nmt", ".cvh");
   scroll_class(id("nav-bar"), "bg-change");
@@ -7,4 +9,15 @@ doc_ready(() => {
   match_media("only screen and (min-width: 576px)", 576, function match() {
     match_height(".mh2");
   });
+
+  add_event(id("g-form"), "submit", () => {
+    submitted = true;
+
+    setTimeout(() => {
+      id("g-form").reset();
+    }, 500);
+  });
+
+  click_copy(id("yahoo"));
+  click_copy(id("gmail"));
 });
