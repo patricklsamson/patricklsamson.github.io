0437fbf34d01b74cdcc2007d674e5469efd8f2ee
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2023-12-30 21:55:13 +0800

    Update resume

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -199,6 +199,24 @@ function page_nav(callback) {
   add_event(id("save-page-native-btn"), "click", function () {
     window.print();
   });
+
+  for (i = 0; i < qsel_all(".nav-span").length; i++) {
+    qsel_all(".nav-span")[i].innerHTML = "page" + (i + 1);
+    add_class(qsel_all(".wrap-paper")[i], "page" + (i + 1));
+
+    add_event(qsel_all(".nav-span")[i], "click", function () {
+      for (i = 0; i < qsel_all(".nav-span").length; i++) {
+        remove_class(qsel_all(".nav-span")[i], "active");
+        remove_class(qsel_all(".wrap-paper")[i], "active");
+      }
+
+      add_class(this, "active");
+      add_class(qsel("." + this.innerHTML), "active");
+      console.log(document.title.split("Resume"))
+      document.title = document.title.split("Resume")[0];
+      document.title += "Resume-" + this.innerHTML.split("e")[1];
+    });
+  }
 }
 
 function fixed_el(
